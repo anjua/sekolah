d0e957d771c1f56bdf9d753412ae09c2a5317582
anjua <anjua.github@gmail.com>
2023-02-09 12:32:53 +0700

    RDbAuthManager Commit

--- protected/modules/rights/RDbAuthManager.php
+++ protected/modules/rights/RDbAuthManager.php
@@ -4,12 +4,42 @@ class RDbAuthManager extends CDbAuthManager
 {
     public $rightsTable = 'Rights';
 
-    private $_item = array();
+    private $_items = array();
     private $_itemChildren = array();
 
     public function addItemChild($itemName, $childName)
     {
         if($this->hasItemChild($itemName, $childName)===false)
             return parent::addItemChild($itemName, $childName);
+    }
+
+    public function assign($itemName, $userId, $bizRule = null, $data = null)
+    {
+        if ($this->getAuthAssignment($itemName, $userId)===null) 
+        {
+            return parent::assign($itemName, $userId, $bizRule, $data);
+        }
+    }
+
+    public function getAuthItem($name, $allowCaching=true)
+    {
+        if ($allowCaching && $this->_items===array()) 
+            $this->_items = $this->getAuthItems();
+
+        if($allowCaching && isset($this->_items[$name]))
+        {
+            return $this->_items[$name];
+        }
+        elseif($item=parent::getAuthItem($name)!==null)
+        {
+            return $item;
+        }
+
+        return null;
+    }
+
+    public function getAuthItemsByNames($names, $nested=false)
+    {
+        
     }
 }
